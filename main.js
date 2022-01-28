4a3613c83a183c9d66b30164e5d78a456bcea8f7
Sigurd B <sigurdboels96@hotmail.com>
2022-01-28 22:59:25 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data:function() {
+        return {
+            product: 'Socks',
+            description: 'Smelly stinky socks'
+            
+        }
+    }
+})
